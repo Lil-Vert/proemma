f97b607353d6ca93e530d9eb9365e750e2c5094f
Dwayne <victormatthew626@gmail.comgit commit -m stating git config --global user.email victormatthew626@gmail.com>
2023-06-03 03:44:46 +0100

    new

--- book/bookApp/form.py
+++ book/bookApp/form.py
@@ -1,25 +1,18 @@
-from django.forms import ModelForm, TextInput,  PasswordInput, EmailInput,  ClearableFileInput, SelectMultiple, Select, EmailInput
+from django.forms import ModelForm, TextInput,  PasswordInput, EmailInput,  ClearableFileInput, SelectMultiple, Select
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from PIL import Image
 from django import forms
 from .models import Post, Comment
-from django.forms import ModelForm, TextInput,  PasswordInput, EmailInput
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+
 
 
 class SingupForm(UserCreationForm):
     email = forms.EmailInput()
-    #image = forms.ImageField()
-    #first_name = forms.CharField(max_length=100)
-
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-       
-
 
 class CreatPostForm(forms.ModelForm):
    class Meta:
